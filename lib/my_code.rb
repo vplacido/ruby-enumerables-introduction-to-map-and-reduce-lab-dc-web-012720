0b508b7713c53595bd94e4e57870feb576e06184
Valentin Placido <valentinplacido25@gmail.com>
2020-01-23 21:29:23 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -9,3 +9,19 @@ end
 def map_to_no_change(source_array)
   source_array
 end
+
+def map_to_double(source_array)
+  newArr = []
+  source_array.each do |item|
+    newArr << item * 2 
+  end 
+  newArr
+end
+
+def map_to_square(source_array)
+  newArr = []
+  source_array.each do |item|
+    newArr << item ** 2 
+  end 
+  newArr
+end
