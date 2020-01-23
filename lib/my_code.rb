e613a8e77161cc1c7c7cfb669bc0e45bd1c11e58
Valentin Placido <valentinplacido25@gmail.com>
2020-01-23 21:49:19 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -27,9 +27,5 @@ def map_to_square(source_array)
 end
 
 def reduce_to_total(source_array, starting_point=0)
-  if !starting_point 
-    return source_array.reduce(0,:+)
-  else 
     source_array.reduce(starting_point, :+)
-  end
 end 
